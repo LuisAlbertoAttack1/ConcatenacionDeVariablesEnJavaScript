5d3e4ede61bcd9e4a3b122c69e648c8974c65e9b
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-08-27 14:34:55 -0600

    NuevoProyect

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let valor1 = "hola";
+let valor2 = "Amigos";
+let Resultado = "Mostramos El Mensaje Concatenado : " + valor1 + " " + valor2;
+//EN JAVASCRIPT SE CONCATENA CON EL SIGNO DE ( + ) O IGUALMENTE SE PUEDE CON ( , )
+//AHORA SOLO MANDAREMOS A IMPRIMIR
+//LA VARIABLE RESULTADO
+console.log(Resultado);
+//Como vemos el resultado si se concatena y mostramos el resultado al final 
